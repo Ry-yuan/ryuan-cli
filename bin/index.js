78cdbceffbdbfae20889e63d2a64cec98493f23b
ry.liang <ry.liang@vipshop.com>
2018-05-08 16:46:04 +0800

    fix :　命令行，模版问题  --v1.0.3

--- bin/index.js
+++ bin/index.js
@@ -6,14 +6,16 @@ const shell = require('shelljs');
 const log = require('tracer').colorConsole()
 
 program
-    .version('1.0.2','-v, --version')
+    .version('1.0.3','-v, --version')
     .description('This is a front - end template that can quickly generate front-end projects.')
 
 program
-    .command('ryuan-cli <tpl> <project>','tpl:gulp or wevpack')
+    .command('ryuan-cli <tpl> <project>','tpl: gulp or webpack')
     .action(function(tpl , project) {
-        log.info('example：ryuan-cli <tpl> <project-name>')
-        if (tpl && project) {
+        log.info('example：ryuan-cli <tpl> <project-name>');
+        console.log(tpl);
+        console.log(project);
+        if ((tpl == 'gulp'||tpl == 'webpack') && project) {
             let pwd = shell.pwd()
             log.info(`The template code is being pulled.Download location:${pwd}/${project}/ ...`)
             clone(`https://github.com/Ry-yuan/${tpl}-workflow`, pwd + `/${project}`, null, function() {
@@ -21,7 +23,7 @@ program
                 log.info('The formwork project is completed.Start your work!')
             })
         } else {
-            log.info('The correct command example：ryuan-cli gulp projectName')
+            log.error('The correct command example：ryuan-cli gulp projectName')
         }
     })
 program.parse(process.argv)
